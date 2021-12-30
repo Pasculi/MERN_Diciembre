f3fd8a6c7ae52bd33d93ea9419b289a0ff76ebd6
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-12-29 22:45:33 -0300

    Cuanto tiempo tomo

--- cuantoTiempoTomo/cuantoTiempo.js
+++ cuantoTiempoTomo/cuantoTiempo.js
@@ -1,30 +1,30 @@
 Number.prototype.isPrime = function () {
     for (let i = 2; i < Math.sqrt(this); i++) {
-      if (this % i === 0) {
-        return false;
-      }
+        if (this % i === 0) {
+            return false;
+        }
     }
     return true;
-  };
+};
 const { performance } = require('perf_hooks');
 const start = performance.now();
 let primeCount = 0;
 let num = 2; // por razones matemáticas, 1 se considera primo
-while( primeCount < 1e4 ) {
-    if( num.isPrime() ) {
+while (primeCount < 1e4) {
+    if (num.isPrime()) {
         primeCount++;
     }
     num++;
 }
-console.log(`The 10,000th prime number is ${num-1}`);
+console.log(`The 10,000th prime number is ${num - 1}`);
 console.log(`This took ${performance.now() - start} milliseconds to run`);
 
 //recursivo*********************************
 function rFib(n) {
-  if (n < 2) {
-    return n;
-  }
-  return rFib(n - 1) + rFib(n - 2);
+    if (n < 2) {
+        return n;
+    }
+    return rFib(n - 1) + rFib(n - 2);
 }
 console.time('rFib');
 rFib(20);
@@ -33,12 +33,12 @@ console.timeEnd('rFib');
 
 //iterativo****************************************
 function iFib(n) {
-  const vals = [0, 1];
-  while (vals.length - 1 < n) {
-    let len = vals.length;
-    vals.push(vals[len - 1] + vals[len - 2]);
-  }
-  return vals[n];
+    const vals = [0, 1];
+    while (vals.length - 1 < n) {
+        let len = vals.length;
+        vals.push(vals[len - 1] + vals[len - 2]);
+    }
+    return vals[n];
 }
 console.time('iFib');
 iFib(20);
