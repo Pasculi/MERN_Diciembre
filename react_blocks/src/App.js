ea20a966a0edabde2b5f2d12e3eabb5c1ff6c2bc
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-12-29 23:56:09 -0300

    React Blocks

---
+++ react_blocks/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Advertisement from './components/Advertisement';
+import Header from './components/Header';
+import Main from './components/Main';
+import Navigation from './components/Navigation';
+import SubContents from './components/SubContents';
+
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Navigation />
+      <Main >
+        <SubContents />
+        <SubContents />
+        <SubContents />
+        <Advertisement />
+      </Main>
+    </div>
+  );
+}
+export default App;
