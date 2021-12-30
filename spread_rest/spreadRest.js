e95210e01b1236363fba75ac45276c587201026c
Jorge Sepúlveda Turpie <jorge.sepulveda.turpie@gmail.com>
2021-12-29 22:50:34 -0300

    Spread & Rest

---
+++ spread_rest/spreadRest.js
@@ -0,0 +1,39 @@
+/*
+const animals = ['horse', 'dog', 'fish', 'cat', 'bird'];
+//const [firstAnimal, secondAnimal] = animals;
+
+const [firstAnimal, secondAnimal, ...otherAnimals] = animals;
+console.log(otherAnimals)
+*/
+
+const person = {
+    firstName: 'Bob',
+    lastName: 'Marley',
+    email: 'bob@marley.com',
+    password: 'sekureP@ssw0rd9',
+    username: 'barley',
+    addresses: [
+        {
+            address: '1600 Pennsylvania Avenue',
+            city: 'Washington, D.C.',
+            zipcode: '20500',
+        },
+        {
+            address: '221B Baker St.',
+            city: 'London',
+            zipcode: 'WC2N 5DU',
+        }
+    ],
+    createdAt: 1543945177623
+};
+
+
+const personCopy = {...person}
+
+console.log(personCopy);
+
+console.log(personCopy === person);
+console.log(personCopy.addresses === person.addresses);
+
+
+
